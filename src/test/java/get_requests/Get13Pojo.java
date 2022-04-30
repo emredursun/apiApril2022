2496b53376688886cee8fbd6bb09330427f5338f
Suleyman Alptekin <slptkn@yahoo.com>
2022-04-30 14:15:26 -0400

    Fifth Commit

---
+++ src/test/java/get_requests/Get13Pojo.java
@@ -0,0 +1,32 @@
+package get_requests;
+
+public class Get13Pojo {
+
+    /*
+        Given
+            https://gorest.co.in/public/v1/users/13
+        When
+            User send GET Request to the URL
+        Then
+            Status Code should be 200
+        And
+            Response body should be like
+            {
+                "meta": null,
+                "data": {
+                    "id": 13,
+                    "name": "Agastya Nehru",
+                    "email": "nehru_agastya@ortiz-stark.net",
+                    "gender": "male",
+                    "status": "active"
+                }
+           }
+    */
+
+    /*
+        To do that task do the followings;
+        1)Check the response body on Postman
+        2)Create Pojo Classes
+        3)Follow the 4 steps in API automation
+     */
+}
