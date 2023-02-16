a9e5eb0dd42a3f748058277edf4e75e9d3f6d6e7
emredursun <info.emredursun@gmail.com>
2023-02-16 18:04:55 +0100

    add Put02 class

--- src/test/java/put_requests/Put02.java
+++ src/test/java/put_requests/Put02.java
@@ -1,14 +1,24 @@
 package put_requests;
 
-public class Put02 {
+import base_urls.DummyRestApiBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.*;
+import utils.JsonUtil;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Put02 extends DummyRestApiBaseUrl {
 
     /*
         URL: https://dummy.restapiexample.com/api/v1/update/21
        HTTP Request Method: PUT Request
        Request body: {
-                        "employee_name": "Tom Hanks",
-                        "employee_salary": 111111,
-                        "employee_age": 23,
+                        "employee_name": "Emre Dursun",
+                        "employee_salary": 48000,
+                        "employee_age": 35,
                         "profile_image": "Perfect image"
                      }
        Test Case: Type by using Gherkin Language
@@ -18,12 +28,64 @@ public class Put02 {
                     {
                         "status": "success",
                         "data": {
-                            "employee_name": "Tom Hanks",
-                            "employee_salary": 111111,
-                            "employee_age": 23,
+                            "employee_name": "Emre Dursun",
+                            "employee_salary": 48000,
+                            "employee_age": 35,
                             "profile_image": "Perfect image"
                         },
-                        "message": "Successfully! Record has been added."
+                        "message": "Successfully! Record has been updated."
                     }
      */
+
+    /*
+        Given
+            https://dummy.restapiexample.com/api/v1/update/21
+
+                   Request body: {
+                        "employee_name": "Emre Dursun",
+                        "employee_salary": 48000,
+                        "employee_age": 35,
+                        "profile_image": "Perfect image"
+                     }
+        When
+	 		I send Put Request to the Url
+	 	Then
+		 	Status code is 200
+
+		And
+            Response body should be like the following
+                    {
+                        "status": "success",
+                        "data": {
+                            "employee_name": "Emre Dursun",
+                            "employee_salary": 48000,
+                            "employee_age": 35,
+                            "profile_image": "Perfect image"
+                        },
+                        "message": "Successfully! Record has been updated."
+                    }
+     */
+
+    @Test
+    public void put01() {
+        //1.Step: Set the URL
+        spec.pathParams("first","update","second",21);
+
+        //2.Step: Set the Expected Data
+        DummyApiDataPojo expectedDummyApiData = new DummyApiDataPojo("Emre Dursun", 48000, 35, "Perfect image");
+        DummyApiResponseBodyPojo expectedDummyApiResponseBody = new DummyApiResponseBodyPojo("success", expectedDummyApiData , "Successfully! Record has been updated.");
+
+        //3.Step:Send PUT Request and get the Response
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedDummyApiData).when().put("/{first}/{second}");
+        DummyApiResponseBodyPojo actualDummyApiResponseBody = JsonUtil.convertJsonToJavaObject(response.asString(),DummyApiResponseBodyPojo.class);
+
+        //4.Step: Do Assertion
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedDummyApiResponseBody.getStatus(), actualDummyApiResponseBody.getStatus());
+        assertEquals(expectedDummyApiData.getEmployee_name(), actualDummyApiResponseBody.getData().getEmployee_name());
+        assertEquals(expectedDummyApiData.getEmployee_salary(), actualDummyApiResponseBody.getData().getEmployee_salary());
+        assertEquals(expectedDummyApiData.getEmployee_age(), actualDummyApiResponseBody.getData().getEmployee_age());
+        assertEquals(expectedDummyApiData.getProfile_image(),actualDummyApiResponseBody.getData().getProfile_image());
+        assertEquals(expectedDummyApiResponseBody.getMessage(), actualDummyApiResponseBody.getMessage());
+    }
 }
